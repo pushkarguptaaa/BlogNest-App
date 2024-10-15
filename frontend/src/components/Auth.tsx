ba93f9aa18d283d54f3d92a57ce32c6b076b3994
Pushkar Gupta <pushkardps8@gmail.com>
2024-10-15 16:25:54 +0530

    Completed Auth Component

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -19,8 +19,8 @@ export const Auth = ({type}: {type: "signup" | "signin"}) => {
                     </div>
 
                     <div className="text-slate-600">
-                        Already have an account? 
-                        <Link className="pl-2 underline" to={"/signin"}>Login</Link>
+                        {type === "signup" ? "Already have an account?" : "Don't have an account?"}
+                        <Link className="pl-2 underline" to={type === "signin" ? "/signup" : "/signin"}>{type === "signin" ? "Sign up" : "Sign in"}</Link>
                     </div>
                 </div>
 
@@ -46,6 +46,8 @@ export const Auth = ({type}: {type: "signup" | "signin"}) => {
                         })
                     }} />
                 </div>
+
+                <button type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
             </div>
             
         </div>
